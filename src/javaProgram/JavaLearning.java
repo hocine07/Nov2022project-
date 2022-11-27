22a3cfb456fce3df27f5dc1734094e2d275b67d4
hocine07 <lhoucoutinho@gmail.com>
2022-11-27 17:05:01 -0500

    first commit

---
+++ src/javaProgram/JavaLearning.java
@@ -0,0 +1,61 @@
+package javaProgram;
+
+public class JavaLearning {
+
+	public static void main(String[] args) {
+		
+   int a=13;
+   int b=14;
+   
+   System.out.println(a+b);
+   System.out.print(a-b);
+   System.out.println(" this is the mul of a and b is :" +(a*b));
+   System.out.println("this is the diff of a and b is :" + (a-b));
+   
+   // relational data types returns only boolean results 
+   System.out.println("-----------boolean--------");
+   
+   
+   System.out.println(a=b);
+   System.out.println(a>b);
+   System.out.println(a<b);
+   System.out.println(a>=b);
+   System.out.println(a<=b);
+   
+   System.out.print("------------ logical operators--------------- & || ! ");
+   // works between two booleans
+   
+   boolean x=true;
+   boolean y=false;
+    
+   System.out.println(x&&y); //false 
+   System.out.println(x||y); //true
+   System.out.println(!x); // false
+   
+    
+   
+   //increment decrement operators
+   
+   
+   a=14;
+   a++;
+   
+   System.out.println(a);
+   
+   
+   b=15;
+   b--;
+   
+   System.out.print(b);
+   
+   
+   
+   System.out.println("hello hocine");
+   
+   
+
+   
+		 
+	} 
+   
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author lhouc
+ *
+ */
+module javaProgram {
+}
